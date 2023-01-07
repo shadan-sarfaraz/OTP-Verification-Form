20e19ef6cd86691277f0fe8521e8694964e74496
Shadan Sarfaraz <shadansarfaraz123@gmail.com>
2023-01-07 10:44:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.querySelectorAll(".code").forEach(function(otpEl) {
+    otpEl.addEventListener("keyup", backSp);
+    otpEl.addEventListener("keypress", function() {
+         
+        var nexEl = this.nextElementSibling;
+        nexEl.focus();
+    })
+})
+
+function backSp(backKey) {
+    if (backKey.keyCode == 8) {
+        var prev = this.previousElementSibling.focus()
+    }
+}
+// OTP Input (End)
